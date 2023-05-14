f3bd176bd48de4ce0c772c2ef9afc9439d082850
NatdanaiMartk <131628899+NatdanaiMartk@users.noreply.github.com>
2023-05-14 15:30:19 +0700

    Add Excerise4

---
+++ Assignment from BorntoDev/Exercise4_Natdanai_M.py
@@ -0,0 +1,32 @@
+'''
+Display Score of
+    Foundation English
+    General Business
+    Introduction to Computer Systems
+    Computer Programing
+----------------------------------
+Notes
+    'Variable List'
+        FE            = (F)oundation (E)nglish
+        GB            = (G)eneral (B)usiness
+        ICS           = (I)ntroduction to (C)omputer (S)ystems
+        CP            = (C)omputer (P)rograming
+        YS            = (Y)our (S)core
+'''
+'Variable'
+YS = "---Your Score---"
+P = "Points"
+'-----------------------------------------'
+'VariableClass'
+FE = "Foundation English"
+GB = "General Business"
+ICS = "Introduction to Computer Systems"
+CP = "Computer Programing"
+'-----------------------------------------'
+
+'---Coding Space---'
+print(YS)
+print(FE, ":", 30.5, P)
+print(GB, ":", 27,   P)
+print(ICS, ":", 42.5, P)
+print(CP, ":", 44,   P)
